c7174dc176bdbf418ae4daa37611033ae3e9dc6e
samuelpondugala <samueldavid.webdev@gmail.com>
2025-10-28 15:41:18 +0530

    First hour of practice done

---
+++ BasicReactApp/src/App.jsx
@@ -0,0 +1,18 @@
+
+const App = () => {
+  return (
+    <>
+      <div id="child1">
+        <h1>App</h1>
+        <h3>Helloooooo</h3>
+      </div>
+    <div id="child2">
+      <h1>2nd child el</h1>
+      <h4>hieee</h4>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
