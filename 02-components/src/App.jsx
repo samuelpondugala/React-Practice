8eb1c0df8449ef964ebc7be7e0e48df71036bdb8
samuelpondugala <samueldavid.webdev@gmail.com>
2025-10-28 18:18:05 +0530

    feat: add NavBar component and integrate it into App

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,9 +1,13 @@
 import Card from "./components/card"
+import NavBar from "./components/navBar"
+
+
 const App = () => {
   const user = "David Samuel" 
   const age = 40
   return (
     <div>
+      <NavBar/>
       <h1>Hello guys, I am {user}</h1>
       <h2>and I am {age} year old</h2>
       <Card/>
